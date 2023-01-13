52b5cf44498862e8cc686d23782377e062389a51
Lekuba Ntoane <l.ntoane@gmail.com>
2023-01-13 12:40:52 +0200

    Selected inguries based on product code and made analysis

--- analysis.R
+++ analysis.R
@@ -12,3 +12,22 @@ products
 # Load population
 population <- vroom::vroom("neiss/population.tsv")
 population
+
+# Injuries with product code 649
+selected <- injuries %>% filter(prod_code == 649)
+nrow(selected)
+
+# Injuries involving toilets most often occur at home by falling, diagonis are varied
+selected %>% count(location, wt = weight, sort = TRUE)
+selected %>% count(body_part, wt = weight, sort = TRUE)
+selected %>% count(diag, wt = weight, sort = TRUE)
+
+# Explore patterns across age and sex
+summary <- selected %>% count(age, sex, wt = weight)
+summary
+
+# Plot the patterns
+summary %>% 
+  ggplot(aes(age, n, colour = sex)) + 
+  geom_line() + 
+  labs(y = "Estimated number of injuries")
